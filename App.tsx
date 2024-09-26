4c8e4faec6774829e0f3f7ed9c6f91c92ea4c148
Ranjan Shrestha <ranjan@sharingapples.com>
2024-09-26 15:05:05 +0545

    Add code to fix full screen keyboard sizing

--- App.tsx
+++ App.tsx
@@ -8,11 +8,13 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  KeyboardAvoidingView,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
 } from 'react-native';
@@ -25,94 +27,25 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: 'blue',
+    flex: 1
+    
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+    <View style={backgroundStyle} behavior='height'>
+      
+      <ScrollView style={{flex: 1, backgroundColor: 'black'}}>
+        <TextInput style={{ backgroundColor: 'green', padding: 50 }} keyboardType='default' />
+        <TextInput style={{ backgroundColor: 'red', padding: 50 }} keyboardType='numeric' />
+
       </ScrollView>
-    </SafeAreaView>
+      <View style={{ height: 300, backgroundColor: 'blue' }} />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- android/app/src/main/java/com/keyboardcheck/MainActivity.java
+++ android/app/src/main/java/com/keyboardcheck/MainActivity.java
@@ -5,7 +5,92 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+import android.os.Bundle;
+import android.view.View;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.os.Build;
+
 public class MainActivity extends ReactActivity {
+  private SoftInputAssist softInputAssist;
+
+  final Runnable hideUI = new Runnable() {
+    @Override
+    public void run() {
+      View decorView = getWindow().getDecorView();
+      getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+      decorView.setSystemUiVisibility(
+              View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                      | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                      | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                      | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                      | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                      | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+
+      decorView.setFitsSystemWindows(true);
+      // getWindow().setFitsSystemWindows(true);
+
+      // getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+    }
+  };
+
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) {
+    super.onWindowFocusChanged(hasFocus);
+    if (hasFocus) {
+      hideUI.run();
+    }
+  }
+
+  @Override
+  public void onCreate(Bundle bundle) {
+    super.onCreate(bundle);
+
+    // For Android P and above, make sure we use the cutout notches as well.
+    // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+    //   WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+    //   layoutParams.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+    //   getWindow().setAttributes(layoutParams);
+    //   // getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    //   // getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+    // }
+
+    final Handler handler = new Handler();
+
+    getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+      public void onSystemUiVisibilityChange(int visibility) {
+        if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+          hideUI.run();
+
+          // handler.postDelayed(hideUI, 0);
+        }
+      }
+    });
+
+    softInputAssist = new SoftInputAssist(this);
+
+  }
+
+  @Override
+  protected void onResume() {
+    softInputAssist.onResume();
+    super.onResume();
+  }
+
+  @Override
+  protected void onPause() {
+    softInputAssist.onPause();
+    super.onPause();
+  }
+
+  @Override
+  public void onDestroy() {
+    softInputAssist.onDestroy();
+    super.onDestroy();
+
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
